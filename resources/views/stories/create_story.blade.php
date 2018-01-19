fba1a5bac5b67e8b22bb2a67c494bc17df8f3e6b
jose casanova <jose@nayraweb.com>
2018-01-19 16:21:31 -0300

    arreglada  funcionalidad en tags

--- resources/views/stories/create_story.blade.php
+++ resources/views/stories/create_story.blade.php
@@ -168,26 +168,45 @@
                     }
                 });
  });
+
+ //on submit
+ $("#story-form").on("submit",function(e){
+   console.log('Submitting');
+   e.preventDefault();
+ });
 </script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.4/js/standalone/selectize.min.js"></script>
 
 <script>
-
-
+//funcionalidades tags
   $('#add_tag').on('click', function(e){
     e.preventDefault();
-    var tag = $('#tag').val();
-
-    $(".tag-group").append('<div class="tag-item"><p>'+tag+'<p><input type="hidden" name="tags[]" value="'+tag+'" /><button>@lang('Eliminar etiqueta')</button></div>');
-    $('#tag').val('');
+    addTag();
     //console.log($select);
   });
-
 
   $('.tag-item').on('click', function(e){
       $(this).remove();
   });
 
+ function addTag(){
+   var tag = $('#tag').val();
+   //no agrega tags vacios
+   if (tag.trim() != '') {
+     $(".tag-group").append('<div class="tag-item"><p>'+tag+'<p><input type="hidden" name="tags[]" value="'+tag+'" /><button>@lang('Eliminar etiqueta')</button></div>');
+     $('#tag').val('');
+   }
+ }
+// permite agregar etiquetas con enter sin afectar al resto del form
+$('html').bind('keypress', function(e)
+{
+   if (e.keyCode == 13) {
+     if($("#tag").is(':focus')){    
+       addTag();
+       return false;
+     }
+   }
+});
 
 
 </script>
