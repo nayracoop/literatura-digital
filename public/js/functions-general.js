c4aba596b58b5fb3d185e4aba0c1537259ed66ad
Marcelo Terreni <materreni@gmx.net>
2018-01-27 16:56:42 -0300

    -Editor de nodos/visualizacion con tab panels, retoque en el footer "always on bottom"

--- public/js/functions-general.js
+++ public/js/functions-general.js
@@ -1,21 +1,33 @@
-$(document).ready(function() {
+$(document).ready(function(){
+
 	$('.navbar-toggle').click(function() {
-        if($('.navbar-toggle').hasClass('on')) {
-            $('.navbar-toggle').removeClass('on');
-        } else {
-            $('.navbar-toggle').addClass('on');
-        }
+	  if($('.navbar-toggle').hasClass('on')){
+	  	$('.navbar-toggle').removeClass('on');
+	  }else{
+	  	$('.navbar-toggle').addClass('on');
+	  }
 	});
 
-	$('.leer, .cerrar-nodo').click(function() {
-        var id = $(this).data('node');		
-        var node = $('#ventana-nodo-' + id);
-
-        if(node.hasClass('esconder')) {
-            node.removeClass('esconder');
-        } else {
-            node.addClass('esconder');
-        }
-        return false;
+	/* BOTONES MODAL */
+	$('.leer, .cerrar-nodo').click(function() {	
+	  if($('.nodo-backdrop').hasClass('esconder')){
+	  	$('.nodo-backdrop').removeClass('esconder');
+	  }else{
+	  	$('.nodo-backdrop').addClass('esconder');
+	  }
+	  return false;
 	});
+
+	/* TABS */	
+	$('.tabs a').click(function(e){
+		e.preventDefault();
+		var tab_id = $(this).attr('href');
+
+		$('.tabs li').removeClass('active');
+		$('.tabpanel').removeClass('active');
+
+		$(this).parent().addClass('active');
+		$(tab_id).addClass('active');
+	})
+
 });
--- resources/views/user/nodes.blade.php
+++ resources/views/user/nodes.blade.php
@@ -19,41 +19,112 @@
             <p class="autor-relato">{{ $story->getAuthorName() }}</p>
           </div>
 
-          <ul class="modos">
-              <li><a href="#">Modo visualización</a></li>
-              <li class="active"><a href="#">Modo listado de nodos</a></li>
+          <ul class="modos tabs" role="tablist">
+            <li class="active"><a href="#modo-visualizacion">Modo visualización</a></li>
+            <li><a href="#modo-listado">Modo listado de nodos</a></li>
           </ul>
 
-          <h1>Listado de nodos</h1>
-          <hr />
+          <div id="modo-visualizacion" class="tabpanel active">  
+            <div class="row" style="clear: both;">
+              <div class="col-sm-7 col-md-6 tit-editor-visual">
+                <h1>Visualización</h1>
+                <hr />
+              </div>
+            </div>
+              
+            <div class="row formulario" style="clear: both;">  
+              <div class="col-sm-7 col-md-6">
+              <style>
+                .ejemplo a {
+                  display: block;
+                  cursor: pointer;
+                  font: 10px sans-serif;
+                  background-color: steelblue;
+                  text-align: right;
+                  padding: 3px;
+                  margin: 1px;
+                  color: white;
+                }
+              </style>
+              <div class="ejemplo"></div>
+              <div class="modal-opciones-nodo">
+                <h2>El hacha</h2>
+                <hr />
+                <a href="#">Leer nodo</a>
+                <a href="#">Editor nodo</a>
+              </div>
+              <script src="d3/d3.v3.min.js" charset="utf-8"></script>
+              <script>
+                  var data = [4, 8, 15, 16, 23, 42];
+                  var x = d3.scale.linear()
+                      .domain([0, d3.max(data)])
+                      .range([0, 100]);
+                  d3.select(".ejemplo")
+                    .selectAll("div")
+                      .data(data)
+                    .enter().append("a")
+                      .style("width", function(d) { return x(d)  + "%"; })
+                      .text(function(d) { return d; });
+              </script>
+              </div>
 
-          <table summary="Lista de nodos del relato">
-          <caption class="invisibilizar">Lista de nodos del relato</caption>
-          <thead>
-            <tr>
-              <th scope="col" class="ordenar"><a href="#">Orden</a></th>
-              <th scope="col"><a href="#">Nombre</a></th>
-              <th scope="col" class="ocultar-sm"><a href="#">Fecha</a></th>
-              <th class="ocultar-lg" scope="col"><a href="#">Caracteres</a></th>
-              <th scope="col"  class="ocultar-sm"><a href="#">Estado</a></th>
-              <th scope="col"><span class="invisibilizar">Editar</span></th>
-            </tr>
-          </thead>
-          <tbody>
-            @foreach( $story->textNodes as $node )
-            <tr>
-              <td>{{$loop->iteration}}</td>
-              <td class="tit-listado">{{$node->title or '--'}}</td>
-              <td class="ocultar-sm">{{ date('d.m.Y', strtotime($node->created_at) ) }}</td>
-              <td class="ocultar-lg">{{ $node->charCount }}</td>
-              <td class="ocultar-sm">Activado</td>
-              <td><a href="{{route('author.story.nodes.edit',[$story->id,$node->id])}}"><button>Editar</button></td>
-            </tr>
-            @endforeach
-          </tbody>
-          </table>
+              <div class="col-sm-4 col-sm-offset-1 col-md-4 col-md-offset-2">
+                <form role="form" class="opciones-visual">
+                  <label for="colores">Colores</label>
+                  <div class="styled-select">
+                    <select type="text" class="form-control" id="colores">
+                      <option>Terroso</option>
+                      <option>Violetas</option>
+                      <option>Fluo</option>
+                      <option>Primarios</option>
+                      <option>Fríos</option>
+                    </select>
+                  </div>           
+                  <div class="container-checkbox">
+                    <div class="check-left">
+                      <div class="check">
+                        <label class="checkbox" for="aleatorio"><input name="aleatorio" checked="checked" type="checkbox" id="aleatorio"><span class="tick"></span>Orden aleatorio</label>
+                      </div>
+                    </div>
+                  </div>
+                </form>  
 
-          <a href="{{route('node.create',$story->_id)}}" ><button class="btn btn-nuevo-relato"><span>Agregar nodo</span><span class="plus"></span></button></a>
+              </div>
+            </div>
+          </div>
+
+          <div id="modo-listado" class="tabpanel">
+            <h1>Listado de nodos</h1>
+            <hr />
+
+            <table summary="Lista de nodos del relato">
+            <caption class="invisibilizar">Lista de nodos del relato</caption>
+            <thead>
+              <tr>
+                <th scope="col" class="ordenar"><a href="#">Orden</a></th>
+                <th scope="col"><a href="#">Nombre</a></th>
+                <th scope="col" class="ocultar-sm"><a href="#">Fecha</a></th>
+                <th class="ocultar-lg" scope="col"><a href="#">Caracteres</a></th>
+                <th scope="col"  class="ocultar-sm"><a href="#">Estado</a></th>
+                <th scope="col"><span class="invisibilizar">Editar</span></th>
+              </tr>
+            </thead>
+            <tbody>
+              @foreach( $story->textNodes as $node )
+              <tr>
+                <td>{{$loop->iteration}}</td>
+                <td class="tit-listado">{{$node->title or '--'}}</td>
+                <td class="ocultar-sm">{{ date('d.m.Y', strtotime($node->created_at) ) }}</td>
+                <td class="ocultar-lg">{{ $node->charCount }}</td>
+                <td class="ocultar-sm">Activado</td>
+                <td><a href="{{route('author.story.nodes.edit',[$story->id,$node->id])}}"><button>Editar</button></td>
+              </tr>
+              @endforeach
+            </tbody>
+            </table>
+          </div>
+
+            <a href="{{route('node.create',$story->_id)}}" ><button class="btn btn-nuevo-relato"><span>Agregar nodo</span><span class="plus"></span></button></a>
 
           <div class="botones-nav-form">
               <a href="#" class="bot ant">Volver a mis relatos</a>
