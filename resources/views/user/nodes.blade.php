89b8b7b7361fede45345fb62d51974bea04475c6
jose casanova <jose@nayraweb.com>
2018-01-29 12:30:40 -0300

    integrando visualizacion en perfil usuario

--- resources/views/user/nodes.blade.php
+++ resources/views/user/nodes.blade.php
@@ -24,74 +24,7 @@
             <li><a href="#modo-listado">Modo listado de nodos</a></li>
           </ul>
 
-          <div id="modo-visualizacion" class="tabpanel active">  
-            <div class="row" style="clear: both;">
-              <div class="col-sm-7 col-md-6 tit-editor-visual">
-                <h1>Visualización</h1>
-                <hr />
-              </div>
-            </div>
-              
-            <div class="row formulario" style="clear: both;">  
-              <div class="col-sm-7 col-md-6">
-              <style>
-                .ejemplo a {
-                  display: block;
-                  cursor: pointer;
-                  font: 10px sans-serif;
-                  background-color: steelblue;
-                  text-align: right;
-                  padding: 3px;
-                  margin: 1px;
-                  color: white;
-                }
-              </style>
-              <div class="ejemplo"></div>
-              <div class="modal-opciones-nodo">
-                <h2>El hacha</h2>
-                <hr />
-                <a href="#">Leer nodo</a>
-                <a href="#">Editor nodo</a>
-              </div>
-              <script src="https://d3js.org/d3.v4.min.js" charset="utf-8"></script>
-              <script>
-                  var data = [4, 8, 15, 16, 23, 42];
-                  var x = d3.scale.linear()
-                      .domain([0, d3.max(data)])
-                      .range([0, 100]);
-                  d3.select(".ejemplo")
-                    .selectAll("div")
-                      .data(data)
-                    .enter().append("a")
-                      .style("width", function(d) { return x(d)  + "%"; })
-                      .text(function(d) { return d; });
-              </script>
-              </div>
-
-              <div class="col-sm-4 col-sm-offset-1 col-md-4 col-md-offset-2">
-                <form role="form" class="opciones-visual">
-                  <label for="colores">Colores</label>
-                  <div class="styled-select">
-                    <select type="text" class="form-control" id="colores">
-                      <option>Terroso</option>
-                      <option>Violetas</option>
-                      <option>Fluo</option>
-                      <option>Primarios</option>
-                      <option>Fríos</option>
-                    </select>
-                  </div>           
-                  <div class="container-checkbox">
-                    <div class="check-left">
-                      <div class="check">
-                        <label class="checkbox" for="aleatorio"><input name="aleatorio" checked="checked" type="checkbox" id="aleatorio"><span class="tick"></span>Orden aleatorio</label>
-                      </div>
-                    </div>
-                  </div>
-                </form>  
-
-              </div>
-            </div>
-          </div>
+          @include('typologies.node_presentation.'.$story->typology)
 
           <div id="modo-listado" class="tabpanel">
             <h1>Listado de nodos</h1>
@@ -144,7 +77,7 @@ $('stories_search').on('submit',function(e){
   e.preventDefault();
 });
 $('input[name="search"]').bind('input',function(){
-  console.log('gato');
+  //console.log('gato');
   formElement = document.getElementById("stories_search");
 
   var xhr = new XMLHttpRequest();
