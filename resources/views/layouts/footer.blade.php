e7b2bf99cb78aaf8c82ef9baad9d642f8f8150ae
jose casanova <jose@nayraweb.com>
2018-02-20 16:02:46 -0300

    incorporando al repo tipografias y logos. primera implementacion de vistas de palabras

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,10 +1,6 @@
 <div class="container-fluid footer-fluid">
-    <footer class="container">
-        <a href="#">
-            <img src="{{ asset('img/logo/logo-untref.png')}}" alt="Universidad Nacional de Tres de Febrero">
-        </a>
-        <a href="#">Maestría en Tecnología
-            <br> y Estética de las
-            <br> Artes Electrónicas</a>
-    </footer>
+  <footer class="container">
+    <a href="#"><img src="img/logo/logo-untref.png" alt="Universidad Nacional de Tres de Febrero"></a>
+    <a href="#">Maestría en Tecnología<br> y Estética de las<br> Artes Electrónicas</a>
+  </footer>
 </div>
--- resources/views/stories/show.blade.php
+++ resources/views/stories/show.blade.php
@@ -1,87 +1,96 @@
 @extends('layouts.main')
-@section('title') 
+@section('title')
   {{ $story->title }} @lang('de') {{ $story->getAuthorName() }}
 @endsection
 
 @section('content')
 <div class="fondo-forms">
     <div class="container">
-      <div class="row">
+      <div class="row leer-palabras">
         <div class="col-md-12">
 
-          <div class="leer-relato">
-            <h1 class="titulo-relato">{{ $story->title }}</h1>
-            <p class="autor-relato">{{ $story->getAuthorName() }}</p>
-          </div>
-          @include('stories.typologies.node_presentation.' . $story->typology)
-            <!--
-            <table summary="Lista">
-            <caption class="invisibilizar">Lista</caption>
-            <thead>
-              <tr>
-                <th scope="col" class="ordenar"><a href="#">Orden</a></th>
-                <th scope="col"><a href="#">Nombre</a></th>
-                <th scope="col" class="ocultar-sm"><a href="#">Fecha</a></th>
-                <th class="ocultar-lg" scope="col"><a href="#">Caracteres</a></th>
-                <th scope="col"><span class="invisibilizar">Leer</span></th>
-              </tr>
-            </thead>
-            <tbody>
-              @forelse($story->textNodes as $node)
-              <tr>
-                <td>{{ $node->_id }}</td>
-                <td class="tit-listado">{{ $node->title }}</td>
-                <td class="ocultar-sm">{{ date('d.m.Y', strtotime($node->published_at) ) }}</td>
-                <td class="ocultar-lg">2000</td>
-                <td><button class="leer" data-node="{{$node->_id}}">Leer</button></td>
-              </tr>
-              @empty
-              <tr>
-                <td colspan="5">No hay fragmentos</td>
-              </tr>
-              @endforelse
+            <div class="palabras">
 
-            </tbody>
-            </table>
--->
-          <div class="botones-nav-form">
-          </div>
+              <h1>{{$story->title}}</h1>
+              <p class="autor">{{$story->getAuthorName()}}</p>
+
+             <ul>
+               @foreach($story->textNodes as $node)
+               <li><a class="leer" id="{{ $node->_id }}"  href="#">{{$node->title}}</a></li>
+               @endforeach
+             </ul>
+
+             </div>
+             <a href="#" class="btn-social">Compartir</a>
+             <a href="#" class="btn-social">Me gusta</a>
+
         </div>
       </div>
     </div>
   </div>
+
+  <div class="nodo-backdrop-fondo esconder">
   @foreach($story->textNodes as $node)
-  <div class="nodo-backdrop esconder" id="ventana-nodo-{{ $node->_id }}"  tabindex="-1" role="dialog" aria-labelledby="tit-nodo-{{ $node->_id}}" aria-hidden="true">
 
-        <a class="back-button cerrar-nodo" data-node="{{$node->_id}}" href="#">Volver</a>
+    <div class="nodo-backdrop" id="ventana-nodo-{{ $node->_id }}" tabindex="-1" role="dialog" aria-labelledby="tit-nodo" aria-hidden="true">
 
-        <div class="nodo-data-relato">
-          <a href="#" class="cerrar-nodo">
-            <div class="image-clip">
-              @if($story->cover != null && !empty($story->cover))
-                  <img alt="@lang('tapa de') {{$story->title}}" src="{{ asset('imagenes/cover/'.$story->cover )}}">
-                  @else
-                  <img alt="" src="{{ asset('img/tapa200x200.png')}}">
-              @endif
-            </div>
-            <p class="tit-relato">{{ $story->title }}</p>
-            <p class="autor-relato">{{ $story->getAuthorName() }}</p>
-          </a>
-        </div>
+          <a class="back-button cerrar-nodo close-arrow" data-node="{{$node->_id}}" href="#">Volver</a>
 
-        <div class="titulo-nodo">
-          <h1 id="tit-nodo-{{ $node->_id}}">{{ $node->title }}</h1>
-          <span class="acento"><span></span></span>
-        </div>
+          <div class="nodo-data-relato">
+            <a href="#" class="cerrar-nodo">
+              <div class="image-clip">
+                @if($story->cover != null && !empty($story->cover))
+                    <img alt="@lang('tapa de') {{$story->title}}" src="{{ asset('imagenes/cover/'.$story->cover )}}">
+                    @else
+                    <img alt="" src="{{ asset('img/tapa200x200.png')}}">
+                @endif
+              </div>
+              <p class="tit-relato">{{ $story->title }}</p>
+              <p class="autor-relato">{{ $story->getAuthorName() }}</p>
+            </a>
+          </div>
 
-        <div class="container-nodo">{!!$node->text!!}</div>
-        <a class="back-button back-button-bottom cerrar-nodo" data-node="{{$node->_id}}" href="#">Volver</a>
-  </div>
+          <div class="titulo-nodo">
+            <h1 id="tit-nodo">{{ $node->title }}</h1>
+            <span class="acento"><span></span></span>
+          </div>
+
+          <div class="container-nodo">{!!$node->text!!}</div>
+
+          <a class="back-button back-button-bottom cerrar-nodo" data-node="{{$node->_id}}" href="#">Volver</a>
+
+    </div>
+
   @endforeach
-
+  </div>
+  @include('snippets.comments.story')
 
 @endsection
 
 @push('javascript')
+<script src="{{asset('js/libs/jquery.min.js')}}"></script>
 
+<script src="{{asset('js/bootstrap.min.js')}}"></script>
+
+<script>
+  var posiciones = [ ['Animales', '120', '230'], ['Descanso', '110', '370'], ['Supersticiones','433','784'], ['Festejos', '75', '530'], ['Ventana', '74', '250'], ['Lluvia', '320', '580'], ['Leña', '450', '190'], ['Tradiciones', '250', '365'], ['Cielo', '480', '488'], [ 'Familia', '240', '466'], ['Interiores', '520', '40'], ['Reuniones', '756', '270'], ['Compañerismo', '248', '638'], ['Albañilería', '156', '545'], ['Cerveza', '688', '780'], ['Árboles', '326', '620'] ];
+
+    var i=0;
+    $('.palabras a').each(function() {
+      var mult = 1;
+      if($(window).width() < 992){
+        var mult = 0.710;
+      }
+      $(this).parent().css({ 'top': + (posiciones[i][1] * mult) + 'px', 'left':  + (posiciones[i][2] * mult)  + 'px' })
+      i++;
+    });
+
+    $('.palabras a').click(function() {
+      if($('.nodo-backdrop-fondo').hasClass('esconder')){
+        $('.nodo-backdrop-fondo').removeClass('esconder');
+        $('body').addClass('overflow');
+      }
+      return false;
+    });
+  </script>
 @endpush
