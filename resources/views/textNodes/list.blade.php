6c7fd2aab4994509f4b35ed99afb6457e0170fa7
pehuen-rodriguez <pehuen.rodriguez@gmail.com>
2018-04-06 18:00:27 -0300

    Mensaje de error si no hay nodos

--- resources/views/textNodes/list.blade.php
+++ resources/views/textNodes/list.blade.php
@@ -18,35 +18,38 @@
             <div id="modo-listado" class="tabpanel">
                 <h1>Listado de nodos</h1>
                 <hr />
-
-                <table summary="Lista de nodos del relato">
-                <caption class="invisibilizar">Lista de nodos del relato</caption>
-                <thead>
-                    <tr>
-                    <th scope="col" class="ordenar"><a href="#">Orden</a></th>
-                    <th scope="col"><a href="#">Nombre</a></th>
-                    <th scope="col" class="ocultar-sm"><a href="#">Fecha</a></th>
-                    <th class="ocultar-lg" scope="col"><a href="#">Caracteres</a></th>
-                    <th scope="col"  class="ocultar-sm"><a href="#">Estado</a></th>
-                    <th scope="col"><span class="invisibilizar">Editar</span></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($story->textNodes as $node)
-                    <tr>
-                        <td>{{$loop->iteration}}</td>
-                        <td class="tit-listado">{{$node->title or '--'}}</td>
-                        <td class="ocultar-sm">{{ date('d.m.Y', strtotime($node->created_at) ) }}</td>
-                        <td class="ocultar-lg">{{ $node->charCount }}</td>
-                        <td class="ocultar-sm">{{ ucfirst($node->status) }}</td>
-                        <td>
-                            <a href="{{ route('node.edit', [$story->slug, $node->id]) }}">
-                                <button>Editar</button>
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-                </table>
+                @if (count($story->textNodes) <= 0)
+                    <div class="aviso">Todavía no creaste nodos para tu relato <strong>¿Qué estás esperando?</strong></div>
+                @else
+                    <table summary="Lista de nodos del relato">
+                    <caption class="invisibilizar">Lista de nodos del relato</caption>
+                    <thead>
+                        <tr>
+                        <th scope="col" class="ordenar"><a href="#">Orden</a></th>
+                        <th scope="col"><a href="#">Nombre</a></th>
+                        <th scope="col" class="ocultar-sm"><a href="#">Fecha</a></th>
+                        <th class="ocultar-lg" scope="col"><a href="#">Caracteres</a></th>
+                        <th scope="col"  class="ocultar-sm"><a href="#">Estado</a></th>
+                        <th scope="col"><span class="invisibilizar">Editar</span></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($story->textNodes as $node)
+                        <tr>
+                            <td>{{$loop->iteration}}</td>
+                            <td class="tit-listado">{{$node->title or '--'}}</td>
+                            <td class="ocultar-sm">{{ date('d.m.Y', strtotime($node->created_at) ) }}</td>
+                            <td class="ocultar-lg">{{ $node->charCount }}</td>
+                            <td class="ocultar-sm">{{ ucfirst($node->status) }}</td>
+                            <td>
+                                <a href="{{ route('node.edit', [$story->slug, $node->id]) }}">
+                                    <button>Editar</button>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                    </table>
+                @endif
             </div>
             <a href="{{ route('node.create', $story->_id) }}"><button class="btn btn-nuevo-relato"><span>{{__('messages.add_node')}}</span><span class="plus"></span></button></a>
             <div class="botones-nav-form">
