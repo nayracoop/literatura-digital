b3e226c4bb45b531b95453eb49685b7d0bfdc5f2
jose casanova <jose@nayraweb.com>
2018-08-22 13:31:32 -0300

    block_list de stories hace tambien la comprobacion de autoria

--- resources/views/stories/block_list.blade.php
+++ resources/views/stories/block_list.blade.php
@@ -1,13 +1,13 @@
 <div class="row">
     <div class="col-sm-12">
         <div class="row">
-            @forelse($stories as $story)
-              @if($isOwner || $story->status === 'publicado')  
-                @include('stories.block_summary')
+          @forelse($stories as $story)
+              @if((Auth::check() && Auth::user()->_id === $story->author_id) || $story->status === 'publicado')
+                  @include('stories.block_summary')
               @endif
-            @empty
-                    <div class="aviso">No se produjeron resultados</div>
-            @endforelse
+          @empty
+              <div class="aviso">No se produjeron resultados</div>
+          @endforelse
         </div>
     </div>
 </div>
