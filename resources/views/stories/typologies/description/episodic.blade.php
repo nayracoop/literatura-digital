49e764e04648cdf2dd9b73742a652cf40f943584
Marcelo Terreni <materreni@gmx.net>
2018-09-02 02:53:06 -0300

    Explicacion de cada categoria con sus imagenes y pequeños cambios de CSS

--- resources/views/stories/typologies/description/episodic.blade.php
+++ resources/views/stories/typologies/description/episodic.blade.php
@@ -9,18 +9,54 @@
         <div class="row">
 
         <div class="col-sm-7">
-            <p>Ahora A... ha penetrado en el cuarto, por la puerta interior que comunica con el pasillo central. A... no mira hacia la ventana, abierta de par en par, por la que -desde la puerta-podría ver ese ángulo de la terraza. Ahora se vuelve hacia la puerta para cerrarla de nuevo. Continúa luciendo el mismo vestido claro, muy ajustado y de cuello recto, que lucía durante el almuerzo. Chirstiane, otra vez más, le ha indicado que ropas menos ceñidas consiguen hacer soportar mejor el calor. Pero A... se ha limitado a sonreír: el calor no la molestaba, pues había conocido climas mucho más cálidos -en África, por ejemplo- y siempre se había encontrado perfectamente en ellos. Por lo demás, tampoco le asustaba el frío. En todos los lugares conservaba la misma naturalidad. Los rizos negros de su cabello se mecen delicados sobre sus hombros y su espalda, cuando gira la cabeza.</p>
+            <p>El formato <strong>episódico</strong> se inspira en la forma que tienen algunos relatos de dividir su estructura en una serie de escenas autoconclusivas e independientes que al mismo tiempo mantienen un valor de conjunto.</p>
 
-            <div class="figure">
+          <p>Algunos académicos sitúan el origen de este tipo de relatos en las formas narrativas propias de las culturas orales que no conocían la escritura. Determinados géneros populares en estas culturas como la epopeya contaban historias de gran extensión divididas en episodios que rara vez mantenían el orden cronológico de los acontecimientos. Estas narraciones nunca se pasaban al medio escrito y la única forma de conservarlas para luego transmitirlas era por medio de la memoria de los propios relatores que escuchaban y asimilaban las historias que habían escuchado de otros. Así, el tipo de estructura episódica se parece más a la forma en que evocamos un recuerdo.</p>
+
+          <p>Muchas de las primeras novelas conservaron varios rasgos de este tipo de estructura. La figura del héroe errante servía para unir los episodios y se verificá tanto en los romances medievales como en <strong>Don Quijote de la Mancha</strong> (1615) de Cervantes y hasta llega a verificarse algunas obras de Dickens como <strong>Los papeles póstumos del Club Pickwick</strong>  (1837) <a class="citame" href="#cita">[1]</a>. El tipo de historia lineal de orden más o menos cronológico que generalmente encontrás cuando ves una película o leés un libro es de adopción más bien reciente y puede decirse que se consolidó con el relato policial a fines del siglo XIX.</p>
+
+
+          <p>Algunos autores actuales siguen usando este tipo de estructura para organizar sus obra. <strong>Suicidio</strong> (2007) del francés Edouard Levé está escrita en segunda persona y se presenta como una descripción de las características, actitudes y escenas de un personaje de ficción que acaba de suicidarse. La novela se estructura en párrafos de colocación casi aleatoria y representan la manera en la que recordamos a alguien, según la metáfora del mismo autor un orden parecido al de "elegir bolitas de una bolsa".</p>
+  
+          <p>Otros escritores llevaron este procedimiento al extremo. En <strong>Composición No.1</strong> (1962), Marc Saporta además de relatar ocho episodios que suceden a lo largo de la vida de un personaje, decide construir un libro sin encuadernación, donde las páginas se ofrecen sueltas para ser mezcladas antes de comenzar el libro. Al pasar cada página el lector va armando los episodios en su cabeza (un accidente de tránsito, una esposa con problemas psiquiátricos, la acción de un grupo de la Resistencia Francesa entre otros) y solo al final logra construir el todo coherente del relato.</p>       
+
+          <div class="figure">
             <img src="{{ asset('img/home/img_7798.jpg') }}" alt="">
-            <p class="epigrafe">A la recia barandilla</p>  
-            </div>
+            <p class="epigrafe">Composición Nº 1 de Marc Saporta en la edición para habla inglesa de <i>Visual Editions</i></p>  
+          </div>
 
-            <p>A la recia barandilla de la balaustrada casin no le queda pintura por la parte superior. El tono grisáseo de la madera puede percibirse en ella estriado por ligeras grietas longitudinales. Al otro lado de esta barandilla, a más de dos metros por debajo del nivel de la terraza, se inicia el huerto.</p>
+          <p>En un sentido parecido funciona <strong>Pecado</strong> (2008) del ruso Zajar Prilepin. Es una novela en relatos donde en un orden no-cronológico presenciamos distintos momentos en la vida de un mismo personaje: en su niñez, cuando es padre o en sus trabajos como sepulturero, personal de seguridad en una discoteca y soldado en Chechenia.<p>
+
+          <div class="figure tam-33">
+            <img src="{{ asset('img/home/perec.jpg') }}" alt="" class="borde">  
+          </div>
+         
+          <p>Otros autores prefieren valerse de metáforas un poco más concretas que funcionan en una manera muy parecida a una novela en episodios. En <strong>La vida instrucciones de uso</strong> (1972)el escritor francés George Perec se vale de la estructura de un edificio parisino para ir alternando las historias del presente y del pasado de cada departamento y sus habitantes.</p>
+
+          <p>En <strong>La maleta</strong> (1983) de Sergei Dovlátov diferentes objetos que salen de una valija (una camisa, unos zapatos o un par de medias) traída por el autor al emigrar de la URSS sucitan una nueva historia.</p>
+
+          <div class="figure tam-33">
+            <img src="{{ asset('img/home/calvino.png') }}" alt="" class="borde">  
+          </div>
+
+          <p>En <strong>El castillo de los destinos cruzados</strong> (1973) de Italo Calvino las historias que componen el libro parten de unas interpretaciones de las cartas del tarot distintas de las que tradicionalmente se les atribuye. La novela se divide en dos partes: El castillo de los destinos cruzados y La taberna de los destinos cruzados. Dos diseños de naipes distintos (el refinado Visconti y el más tosco Marsella) le sirven a Calvino para contar en el primer libro historias de la nobleza y en el segundo relatos de gente corriente.</p>
+
+          <div class="figure tam-33">
+            <img src="{{ asset('img/home/wilson.png') }}" alt="" class="borde">  
+          </div>
+
+          <p>Un último grupo de obras tomaron de modelo estructuras un poco más abstractas que también funcionan a la manera de episodios. El libro <strong>Leñador</strong> (2013) de Mike Wilson presenta una serie de continua de términos relacionados con la vida de los leñadores como <i>hacha</i>, <i>trenzador</i> o <i>cerveza</i>. La explicación de cada palabra se desarrolla con un registro similar al de las enciclopedias. La suma de todos estos conceptos desarrollados a lo largo de más de 400 páginas logra condensar la riqueza de la vida en el bosque.</p>
+
+          <p>En el <strong>Diccionario Jázaro</strong> (1982) la estructura propia de un diccionario le permite a su autor Milorad Pávic ordenar la historia ficticia del pueblo jázaro. En la ficción del libro, los jázaros habrían recibido la influencia de algunas de las tres religiones monoteistas más importantes: la católica, el judaísmo o el islam. Pavic incluye un diccionario distinto para cada culto que explica por medio de entradas de diccionario la historia de determinados mitos y personajes que pertenecieron a ese pueblo. A través de los términos comunes entre las tres partes se figura la influencia cultural de cada una de esa religiones para explicar la historia.</p>
+
 
             <div class="empeza">
             <a href="{{ route('register') }}" class="btn">¡Empezá a escribir!</a>
             </div>
+
+          <div id="cita" class="notapie">
+            <p>[1] Para conocer más de este tema te recomendamos leer <strong>"Oralidad y escritura. Tecnologías de la palabra"</strong> (FCE, 1982) de Walter J. Ong</p>
+          </div>
 
         </div>
 
---
+++ resources/views/stories/typologies/description/temporal.blade.php
@@ -0,0 +1,89 @@
+<div class="fondo-forms">
+    <div class="container explicacion">
+        <div class="row">         
+        <div class="col-sm-12">
+            <a class="back-button" href="{{ URL::previous() }}">Volver</a>
+            <h1>Temporal</h1>
+        </div>
+        </div>
+        <div class="row">
+
+        <div class="col-sm-7">
+          <p>El formato <strong>temporal</strong> puede ser una muy buena gimnasia para inciarse en el ejercicio de la escritura. <a class="citame" href="#cita">[1]</a></p>
+
+                    <h2>Cómo y cuándo escribir</h2>
+
+            <p>La literatura es una actividad que requiere de dos cosas: estar dispuesto a escribir mucho y leer todavía más. Escribir todos los días es una excelente manera de aprender a soltarse y adquirir experiencia con el manejo del lenguaje.</p>
+
+            <p>Podés probar con sesiones cortas de <strong>no más de 20 o 30 minutos</strong>. Es una medida de tiempo posible y fácil de alternar con otras actividades. Algunos prefieren hacerlo temprano apenas se levantan, otros se sienten más cómodos por la noche antes de ir a acostarse. La brevedad es clave para poder ajustarse a esos momentos menos rutinarios y más ocasionales, como cuando conseguís un asiento en el colectivo.</p>
+
+            <p>Escribir un poco todos los días te va a ayudar a acumular material que te puede ser muy útil para futuros proyectos.</p>
+
+            <div class="figure tam-33">
+              <img src="{{ asset('img/home/tweets.jpg') }}" alt="" class="borde">
+            </div>
+
+
+            <p>En 2015 la escritora norteamericana Mira Gonzalez publicó una selección de los tweets que aparecieron en las tres cuentas de Twitter que administró entre 2010 y 2014. El libro se llama <strong>Selected Tweets</strong> (<i>Tweets seleccionados</i>) y también incluye una segunda parte con posteos de otro escritor de alt-lit Tao Lin.</p>
+
+            <h2>Sobre qué escribir</h2>
+ 
+            <p>Una destreza útil y facil de aprender es el de la <strong>escritura libre</strong>. Todo lo que tenés que hacer es hacerte unos minutos y escribir cualquier cosa que te venga a la mente, sin interrupción. Aunque lo que te salgan sean pavadas, no importa, no pares de escribir. Tampoco te pongas a editar lo que ya escribiste, ni vuelvas sobre tus pasos. Si alguien te interrumpe, escribí también esa interrupción. <strong>Lo importante es seguir escribiendo.</strong></p>
+
+            <p>Es posible que al día siguiente o a la media hora pienses que eso que publicaste es una estupidez. No te preocupes, recordá que <strong>Lexia</strong> siempre permite dejar textos en borrador. ¡Y no lo borres! Es posible que eso que escribiste hoy te sea útil en el futuro.</p>
+                    
+            <p>Como alternativa, podés intentar <strong>describir lo mejor posible</strong> un objeto, un cuadro o la habitación en la que estás. Algunos estilos narrativos como la <a href="https://debedehaber.wordpress.com/2012/01/20/el-nouveau-roman-una-teoria-de-la-novela-moderna/">nouveau roman</a> se apoyan muchísimo en este tipo de escritura.</p>
+
+            <p>También podés ayudarte iniciando tu relato con alguna de las <strong>frases gancho</strong> de esta lista:</p>
+                    
+            <ul>
+             <li>"Me acuerdo la ultima vez que..."</li>
+             <li>"Estábamos equivocados..."
+             <li>"El dolor era insoportable."</li>
+             <li>"Esta vez va a ser diferente."</li>
+             <li>"Nuestra única esperanza..."</li>
+             <li>"Me di cuenta que había cometido un terrible error cuando..."</li>
+             <li>"Tenía gusto a..."</li>
+             <li>"Estar equivocado es dificil porque..."</li>
+             <li>"Lo odio porque..."</li>
+             <li>"Nos hizo rerir porque..."</li>
+             <li>"Me arrepiento..."</li>
+             <li>"Cuando me desperté eran las 3 de la mañana..."</li>
+             <li>"El peor dìa de mi vida fue cuando..."</li>
+            </ul>
+
+            <p>Otra opción es contar tu día anterior, una película que viste o relatar un proceso cualquiera a medida que los desarrollás.</p>
+
+          <div class="figure tam-33">
+            <img src="{{ asset('img/home/levrero.jpg') }}" alt="">
+          </div>
+
+          <p>Algo por el estilo hizo el escritor uruguayo Mario Levrero en <strong>La novela luminosa</strong> (2005). El autor uruguayo se embarcó en la tarea imposible de escribir una novela donde ciertas experiencias extraordinarias (que él denominaba <i>"luminosas"</i>) debían pasarse al formato novela sin que en el camino pierdan su condición de tales. Más de dos tercios del libro están ordenados en forma de diario y detallan el proceso de escritura y sus dificultades, sus manías, sus procastinaciones, su adicción a las computadoras, su hipocondría, su relación con las mujeres y por sobre toda las cosas el testimonio minucioso de un gran fracaso.</p>
+
+          <div class="empeza">
+            <a href="{{ route('register') }}" class="btn">¡Empezá a escribir!</a>
+          </div>
+
+          <div id="cita" class="notapie">
+            <p>[1] La mayoría de estas propuestas están adaptadas del libro <strong>“A Handbook for the Productive Writer”</strong> de Bryan Collins.</p>
+          </div>
+
+        </div>
+
+        <div class="col-sm-4  col-sm-offset-1 modelos">
+
+        <div class="podra">
+            <h2>Tu relato se verá de esta manera</h2>
+            <hr />
+        </div>
+        
+        <div class="figure">
+            <img src="{{ asset('img/home/ejemplo-temporal.png') }}" alt="">
+            <p class="epigrafe">Calendario</p>  
+        </div>
+        
+        </div>
+
+        </div>
+    </div>
+</div>
